f35110aba43028c13a717d103b93e63535fd4f41
AnnikaHaug <88134440+AnnikaHaug@users.noreply.github.com>
2021-09-20 15:55:56 +0200

    API update on forecast

--- src/script.js
+++ src/script.js
@@ -61,7 +61,8 @@ cityForm.addEventListener("submit", eventCity);
 doItForTheCity("Stuttgart", "DE", 0);
 
 //Forecast
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -118,6 +119,13 @@ let unitFahrenheit = document.querySelector("#fahrenheit");
 unitFahrenheit.addEventListener("click", fahrenheitClick);
 
 //API Call
+function getForecast(coordinates){
+  console.log(coordinates);
+  let apiKey = "91b45ef981adb0448983aaf0bd46bb46";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showTemperature(response) {
   celsiusTemperature = Math.round(response.data.main.temp);
 
@@ -146,7 +154,7 @@ function showTemperature(response) {
     response.data.timezone
   );
 
-  displayForecast();
+  getForecast(response.data.coord);
 }
 
 function currentCity() {
